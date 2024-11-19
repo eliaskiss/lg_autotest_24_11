2794f969f87ca6d1069ef80d8f23e0a1874fb1f6
elias.kim <elias.kim>
2024-11-20 08:57:45 +0900

    Day1

---
+++ elias/day_2/lec_pillow/lec_pillow.py
@@ -0,0 +1,63 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageFilter
+from PIL import ImageOps
+
+import os
+from enum import Enum
+from icecream import ic
+from datetime import datetime
+
+ic.configureOutput(includeContext=True)
+
+class FILTER(Enum):
+    BLUR = 0
+    CONTOUR = 1
+    DETAIL = 2
+    EDGE_ENHANCE = 3
+    EDGE_ENHANCE_MORE = 4
+    EMBOSS = 5
+    FIND_EDGES = 6
+    SHARPEN = 7
+    SMOOTH = 8
+    SMOOTH_MORE = 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
