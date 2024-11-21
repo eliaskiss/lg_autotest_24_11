62b706b171a78f14ddecbfa1b0d234efeee188f5
elias.kim <eliaskiss@hotmail.com>
2024-11-21 14:29:12 +0900

    Day3

--- elias/day_3/lec_pymysql/lec_pymysql.py
+++ elias/day_3/lec_pymysql/lec_pymysql.py
@@ -36,6 +36,106 @@ class Database:
             message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
             ic(message)
 
+    ################################################################
+    # Execute Only
+    ################################################################
+    def execute_only(self, sql, values=None):
+        try:
+            # select * from elias;
+            # select * from elias where name = "kim" and age = 20;
+            # name = "kim"
+            # age = 20
+            # sql = 'select * from elias where name = %s, and age = %s;'
+            # values = (name, age)
+            # sql = f'select * from elias where name = "{name}", and age = {age};'
+            if self.conn is not None:
+                if values is None:
+                    self.cursor.execute(sql)
+                else:
+                    self.cursor.execute(sql, values)
+            else:
+                ic('DB is not connected!!!')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+    ################################################################
+    # Execute And Commit (Insert, Update, Delete, ...)
+    ################################################################
+    def execute_and_commit(self, sql, values=None):
+        try:
+            if self.conn is not None:
+                self.execute_only(sql, values)
+                self.conn.commit()
+            else:
+                ic('DB is not connected!!!')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+    ################################################################
+    # Commit Only
+    ################################################################
+    def commit_only(self, sql, values=None):
+        try:
+            if self.conn is not None:
+                self.conn.commit()
+            else:
+                ic('DB is not connected!!!')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+    ################################################################
+    # Execute and Return All
+    ################################################################
+    def execute_and_return(self, sql, values=None):
+        try:
+            if self.conn is not None:
+                self.execute_only(sql, values)
+                data_list = self.cursor.fetchall()
+                return data_list
+            else:
+                ic('DB is not connected!!!')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+    ################################################################
+    # Execute and Return One
+    ################################################################
+    def execute_and_return_one(self, sql, values=None):
+        try:
+            if self.conn is not None:
+                self.execute_only(sql, values)
+                data = self.cursor.fetchone()
+                return data
+            else:
+                ic('DB is not connected!!!')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+    ################################################################
+    # Disconnect
+    ################################################################
+    def disconnect_db(self):
+        try:
+            if self.conn is not None:
+                self.conn.close()
+                self.conn = None
+                self.cursor = None
+            else:
+                ic('DB is not connected!!!')
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            ic(message)
+
+if __name__ == '__main__':
+    # DB 객체 생성 및 연결
+    db = Database(host='211.169.249.211', user='dbuser', passwd='dbuser', db='lg_autotest')
+    db.connect_db()
+
 
 
 
