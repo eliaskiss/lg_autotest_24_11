70e8e3be666bfe80f4f085ae011c4d16d377325c
elias.kim <eliaskiss@hotmail.com>
2024-11-22 11:23:53 +0900

    Day3

--- elias/day_4/lec_flask/lec_flask.py
+++ elias/day_4/lec_flask/lec_flask.py
@@ -25,6 +25,72 @@ file_handler = logging.FileHandler(file_name)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+parser = reqparse.RequestParser()
+
+# 127.0.0.1:5000/rest
+@api.route('/rest')
+class Rest(Resource):
+    def sum(self, params):
+        a = params['a']
+        b = params['b']
+
+        return {'reuslt': 'OK', 'sum': a+b}
+
+    def minus(self, params):
+        a = params['a']
+        b = params['b']
+
+        return {'result': 'OK', 'minus':a - b}
+
+    def multi(self, params):
+        a = params['a']
+        b = params['b']
+
+        return {'result': 'OK', 'multi':a * b}
+
+    def devide(self, params):
+        a = params['a']
+        b = params['b']
+
+        return {'result': 'OK', 'devide':a / b}
+
+    #######################################################################################
+
+    def put(self):
+        pass
+
+    def get(self):
+        pass
+
+    def post(self):
+        try:
+            params = request.json.get('params')
+            command = params['command']
+
+            if command == 'sum':
+                resp = self.sum(params)
+            elif command == 'minus':
+                resp = self.minus(params)
+            elif command == 'multi':
+                resp = self.multi(params)
+            elif command == 'devide':
+                resp = self.devide(params)
+            else:
+                resp = {'result': 'fali', 'error': f'{command}는 지원하지 않습니다!!!'}
+
+            return resp
+
+        except Exception as e:
+            message = f'--> Exception is {e} (Line: {sys.exc_info()[-1].tb_lineno})'
+            logger.error(message)
+            return {'result': 'fail', 'error': message}
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 
